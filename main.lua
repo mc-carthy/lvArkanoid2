b30da9f7fb13123577ff874fca9de35fe8ad0898
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-03-02 20:14:09 +0000

    Create array of bricks and create/draw walls

--- main.lua
+++ main.lua
@@ -13,25 +13,38 @@ platform.width = 70
 platform.height = 20
 
 local bricks = {}
-bricks.current_level_bricks = {}
+bricks.rows = 8
+bricks.columns = 11
+bricks.top_left_position_x = 70
+bricks.top_left_position_y = 50
 bricks.brick_width = 50
 bricks.brick_height= 30
+bricks.horizontal_distance = 10
+bricks.vertical_distance = 15
+bricks.current_level_bricks = {}
+
+local walls = {}
+walls.wall_thickness = 20
+walls.current_level_walls = {}
+
 
 function love.load()
-    bricks.add_to_current_level_bricks(bricks.new_brick(100, 100))
-    bricks.add_to_current_level_bricks(bricks.new_brick(160, 100))
+    bricks.construct_level()
+    walls.construct_walls()
 end
 
 function love.update(dt)
     ball.update(dt)
     platform.update(dt)
     bricks.update(dt)
+    walls.update(dt)
 end
 
 function love.draw()
     ball.draw()
     platform.draw()
     bricks.draw()
+    walls.draw()
 end
 
 function love.keypressed(key)
@@ -61,6 +74,17 @@ end
 
 function platform.draw()
     love.graphics.rectangle("line", platform.position_x, platform.position_y, platform.width, platform.height)
+end
+
+function bricks.construct_level()
+    for row = 1, bricks.rows do
+        for col = 1, bricks.columns do
+            local new_brick_position_x = bricks.top_left_position_x + (col - 1) * (bricks.brick_width + bricks.horizontal_distance)
+            local new_brick_position_y = bricks.top_left_position_y + (row - 1) * (bricks.brick_height + bricks.vertical_distance)
+            local new_brick = bricks.new_brick(new_brick_position_x, new_brick_position_y)
+            bricks.add_to_current_level_bricks(new_brick)
+        end
+    end
 end
 
 function bricks.new_brick(position_x, position_y, width, height)
@@ -93,5 +117,65 @@ end
 function bricks.draw()
     for _, brick in pairs(bricks.current_level_bricks) do
         bricks.draw_brick(brick)
+    end
+end
+
+function walls.new_wall(position_x, position_y, width, height)
+    return {
+        position_x = position_x,
+        position_y = position_y,
+        width = width,
+        height = height
+    }
+end
+
+function walls.construct_walls()
+    local left_wall = walls.new_wall(
+        0,
+        0,
+        walls.wall_thickness,
+        love.graphics.getHeight()
+    )
+    local right_wall = walls.new_wall(
+        love.graphics.getWidth() - walls.wall_thickness,
+        0,
+        walls.wall_thickness,
+        love.graphics.getHeight()
+    )
+    local top_wall = walls.new_wall(
+        0,
+        0,
+        love.graphics.getWidth(),
+        walls.wall_thickness
+    )
+    local bottom_wall = walls.new_wall(
+        0,
+        love.graphics.getHeight() - walls.wall_thickness,
+        love.graphics.getWidth(),
+        walls.wall_thickness
+    )
+    walls.current_level_walls['left'] = left_wall
+    walls.current_level_walls['right'] = right_wall
+    walls.current_level_walls['top'] = top_wall
+    walls.current_level_walls['bottom'] = bottom_wall
+end
+
+function walls.update_wall(dt, single_wall)
+
+end
+
+function walls.update(dt)
+    for _, w in pairs(walls.current_level_walls) do
+        walls.update_wall(dt, w)
+    end
+end
+
+function walls.draw_wall(single_wall)
+    love.graphics.rectangle("line", single_wall.position_x, single_wall.position_y, single_wall.width, single_wall.height)
+end
+
+function walls.draw()
+    for _, w in pairs(walls.current_level_walls) do
+        walls.draw_wall(w)
     end
 end
