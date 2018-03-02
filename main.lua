bbfcf103d0c7390948f478ef63b24f62c37dd539
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-03-02 21:45:54 +0000

    Add AABB collision detection functions

--- main.lua
+++ main.lua
@@ -27,6 +27,8 @@ local walls = {}
 walls.wall_thickness = 20
 walls.current_level_walls = {}
 
+local collisions = {}
+
 
 function love.load()
     bricks.construct_level()
@@ -38,6 +40,7 @@ function love.update(dt)
     platform.update(dt)
     bricks.update(dt)
     walls.update(dt)
+    collisions.resolve_collisions(dt)
 end
 
 function love.draw()
@@ -178,4 +181,97 @@ function walls.draw()
     for _, w in pairs(walls.current_level_walls) do
         walls.draw_wall(w)
     end
+end
+
+function collisions.check_rectangles_overlap(a, b)
+    local overlap = false
+    if not (a.x + a.width < b.x or b.x + b.width < a.x or a.y + a.height < b.y or b.y + b.height < a.y) then
+        overlap = true
+    end
+    return overlap
+end
+
+function collisions.ball_platform_collision(ball, platform)
+    local a = {
+        x = platform.position_x,
+        y = platform.position_y,
+        width = platform.width,
+        height = platform.height
+    }
+    local b = {
+        x = ball.position_x - ball.radius,
+        y = ball.position_y - ball.radius,
+        width = 2 * ball.radius,
+        height = 2 * ball.radius
+    }
+    if collisions.check_rectangles_overlap(a, b) then
+        print("Ball-platform collision!")
+    end
+end
+
+function collisions.ball_walls_collision(ball, walls)
+    local b = {
+        x = ball.position_x - ball.radius,
+        y = ball.position_y - ball.radius,
+        width = 2 * ball.radius,
+        height = 2 * ball.radius
+    }
+    for _, wall in pairs(walls.current_level_walls) do
+        local a = {
+            x = wall.position_x,
+            y = wall.position_y,
+            width = wall.width,
+            height = wall.height
+        }
+        if collisions.check_rectangles_overlap(a, b) then
+            print("Ball-wall collision!")
+        end
+    end
+end
+
+function collisions.ball_bricks_collision(ball, bricks)
+    local b = {
+        x = ball.position_x - ball.radius,
+        y = ball.position_y - ball.radius,
+        width = 2 * ball.radius,
+        height = 2 * ball.radius
+    }
+    for _, brick in pairs(bricks.current_level_bricks) do
+        local a = {
+            x = brick.position_x,
+            y = brick.position_y,
+            width = brick.width,
+            height = brick.height
+        }
+        if collisions.check_rectangles_overlap(a, b) then
+            print("Ball-brick collision!")
+        end
+    end
+end
+
+function collisions.platform_walls_collision(platform, walls)
+    local b = {
+        x = platform.position_x,
+        y = platform.position_y,
+        width = platform.width,
+        height = platform.height
+    }
+    for _, wall in pairs(walls.current_level_walls) do
+        local a = {
+            x = wall.position_x,
+            y = wall.position_y,
+            width = wall.width,
+            height = wall.height
+        }
+        if collisions.check_rectangles_overlap(a, b) then
+            print("Platform-wall collision!")
+        end
+    end
+end
+
+function collisions.resolve_collisions()
+    collisions.ball_platform_collision(ball, platform)
+    collisions.ball_walls_collision(ball, walls)
+    collisions.ball_bricks_collision(ball, bricks)
+    collisions.platform_walls_collision(platform, walls)
 end
